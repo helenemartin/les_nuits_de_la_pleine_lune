8c647e686e0d10bce395e880f422fcb38ce6a6fc
Helene Martin <helenemartin13@hotmail.com>
2013-08-14 21:08:01 +0100

    pg instead of sqlite

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,5 +1,9 @@
 class HomePagesController < ApplicationController
   def index
   	@moon = Moon.new
+  	
+  	unless @moon.phase == :full
+  	  render text: "not allowed" and return
+  	end
   end
 end
